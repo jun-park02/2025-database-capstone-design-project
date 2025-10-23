0d6c1888940a3d83356b645b432a03075dc9d2bc
jhpark <applestrawberryg@gmail.com>
2025-10-23 17:18:46 +0900

    jwt 토큰 임시

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+import datetime
 
 app = Flask(__name__)
+
+# 나중에 변경, config로 분리
+JWT_SECRET_KEY = "my_secret_key"
+JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(hours=3)
+JWT_REFRESH_TOKEN_EXPIRES = datetime.timedelta(days=7)
 
 @app.route("/")
 def hello_world():
     return "hello world"
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method
 
 if __name__ == "__main__":
     app.run(debug=True)
