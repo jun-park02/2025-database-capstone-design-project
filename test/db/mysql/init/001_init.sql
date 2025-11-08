b78223bfd1a8d12272700d4adbd16cccc081765e
junhyeok park <applestrawberryg@gmail.com>
2025-11-08 16:33:48 +0900

    SQL 문 수정

--- test/db/mysql/init/001_init.sql
+++ test/db/mysql/init/001_init.sql
@@ -2,7 +2,7 @@ USE appdb;
 
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` VARCHAR(255) NOT NULL,        
-  `password` VARCHAR(255) NOT NULL,                         
+  `password_hash` VARCHAR(255) NOT NULL,                         
   `user_email` VARCHAR(255) NOT NULL,                       
   `status` TINYINT UNSIGNED NOT NULL DEFAULT 1,             
   `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
